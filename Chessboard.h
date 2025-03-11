36a307e953fe02e5abc4d6b4fe565683c30ce89a
Mire <148487050+RomaMire@users.noreply.github.com>
2025-03-11 18:54:30 +0100

    Test and check mate

--- Chessboard.h
+++ Chessboard.h
@@ -19,12 +19,25 @@ class Chessboard
 protected:
     Figure* Board [8][8];
 
+    int whiteKingX, whiteKingY; //white king position
+    int blackKingX, blackKingY; //black king position
+
 public:
     Chessboard();
     ~Chessboard();
 
     void showChessboard();
     void addArmy();
+
+    bool moveFigure(int startX, int startY, int endX, int endY);
+
+
+    void checkKingPosition();// Search king's position
+
+
+    bool isKingInCheck(int team) ;//Check if the king is in check
+
+    bool isCheckmate(int team);
 };
 
 #endif // CHESSBOARD_H
