460d3294b54716a716944f5797765f9cecf4f20d
Mire <148487050+RomaMire@users.noreply.github.com>
2025-03-18 20:50:26 +0100

    first castling version
    first castling version - check if K or Rook was moved

--- King.h
+++ King.h
@@ -5,9 +5,18 @@
 
 class King : public Figure
 {
+private:
+     bool wasChecked = false;
+
 public:
     King(char kind, int team);
     ~King();
+
+    // check if king was in check - castling possibility
+    bool wasInCheck();
+    void setChecked();
+
+    bool isMoveValid(int startX, int startY, int endX, int endY, Figure* board[8][8]) override;
 };
 
 #endif // KING_H
--- main.cpp
+++ main.cpp
@@ -15,6 +15,38 @@ int main(int argc, char *argv[])
     Board.addArmy();
 
     Board.showChessboard();
+//checking move
+    Board.moveFigure(1,4,3,4);
+    Board.showChessboard();
+    Board.moveFigure(0,4,1,4);
+    Board.moveFigure(1,4,2,4);
+
+
+   Board.moveFigure(6,3,4,3);//white pawn before attack
+
+   Board.moveFigure(4,3,3,3);//check by white pawn
+   //Board.moveFigure(1,0,2,0);// move other black figure?//******** working!
+  // Board.moveFigure(3,3,2,4);  //pawn attack on king
+
+   Board.moveFigure(2,4,1,4); //king one back
+  // Board.moveFigure(1,4,2,4); //king one forward
+
+  //Board.moveFigure(7,6,5,5);//horse white
+  /*  Board.moveFigure(5,5,3,6);
+
+    Board.moveFigure(1,0,2,0);
+
+*/
+   // Board.moveFigure(1,0,2,0);
+
+   //Board.moveFigure(1,4,2,3);//blac king attack ok
+   // Board.moveFigure(1,2,3,2); //able to hit
+
+   Board.moveFigure(3,3,2,3);//white pawn one forward - check
+
+  // Board.moveFigure(1,0,2,0);// move other black figure?//******** working!
+
+    Board.showChessboard();
 
     QApplication a(argc, argv);
     MainWindow w;
